c296b30aa880c13f3d4f913a524026c470bec06a
bdvalero98 <danilovalero25@gmail.com>
2024-11-26 21:32:23 -0500

    Agregando condicionales

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
-	"AprendiendoGo/variables"
 	"fmt"
+	"runtime"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(3529)
-	fmt.Println(estado)
-	fmt.Println(texto)
+	//estado, texto := variables.ConviertoaTexto(3529)
+	//fmt.Println(estado)
+	//fmt.Println(texto)
+
+	if os := runtime.GOOS; os == "linux." || os == "OS X." {
+		fmt.Println("Esto no es Windows es: ", os)
+	} else {
+		fmt.Println("Esto es Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
